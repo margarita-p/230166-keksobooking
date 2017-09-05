90a953139feacbe571249fc705dc397b826d6c54
margarita-p <dayzzzy@gmail.com>
2017-09-05 18:44:24 +0300

    Задание 18-2

--- js/form.js
+++ js/form.js
@@ -12,29 +12,86 @@
   var priceFieldValues = [1000, 0, 5000, 10000];
 
   var roomNumberField = noticeForm.querySelector('#room_number');
-  var roomNumberFieldValues = ['1', '2', '3', '100'];
   var capacityField = noticeForm.querySelector('#capacity');
-  var capacityFieldValues = ['1', '2', '3', '0'];
 
-  var onTimeInFieldEvent = function () {
+  var titleField = noticeForm.querySelector('#title')
+  var addressField = noticeForm.querySelector('#address')
+
+  var onTimeInFieldChange = function () {
     window.synchronizeFields.equalValues(timeInField, timeOutField);
   };
 
-  var onTimeOutFieldEvent = function () {
+  var onTimeOutFieldChange = function () {
     window.synchronizeFields.equalValues(timeOutField, timeInField);
   };
 
-  var onTypeFieldEvent = function () {
-    window.synchronizeFields.cycleValues(typeField, typeFieldValues, priceField, priceFieldValues);
+  var onTypeFieldChange = function () {
+    window.synchronizeFields.minValues(typeField, typeFieldValues, priceField, priceFieldValues);
   };
 
-  var onRoomNumberFieldEvent = function () {
-    window.synchronizeFields.cycleValues(roomNumberField, roomNumberFieldValues, capacityField, capacityFieldValues);
+  var onRoomNumberFieldChange = function () {
+    window.synchronizeFields.indexValues(roomNumberField, capacityField);
   };
 
-  timeInField.addEventListener('change', onTimeInFieldEvent);
-  timeOutField.addEventListener('change', onTimeOutFieldEvent);
-  typeField.addEventListener('change', onTypeFieldEvent);
-  roomNumberField.addEventListener('change', onRoomNumberFieldEvent);
+  var onCapacityFieldFieldChange = function () {
+    window.synchronizeFields.indexValues(capacityField, roomNumberField);
+  };
+
+  timeInField.addEventListener('change', onTimeInFieldChange);
+  timeOutField.addEventListener('change', onTimeOutFieldChange);
+  typeField.addEventListener('change', onTypeFieldChange);
+  roomNumberField.addEventListener('change', onRoomNumberFieldChange);
+  capacityField.addEventListener('change', onCapacityFieldFieldChange);
+
+  // Валидация заголовка
+  titleField.addEventListener('invalid', function () {
+    if (!titleField.validity.valid) {
+      if (titleField.validity.tooShort) {
+        titleField.setCustomValidity('От 30 до 100');
+      } else if (titleField.validity.tooLong) {
+        titleField.setCustomValidity('От 100 до 30');
+      } else if (titleField.validity.valueMissing) {
+        titleField.setCustomValidity('Тут надо обязательно написать заголовок, иначе никак');
+      }
+    } else {
+      titleField.setCustomValidity('');
+    }
+  });
+
+  // Валидация заголовка для Edge
+  titleField.addEventListener('input', function (evt) {
+    var target = evt.target;
+    if (target.value.length < 30) {
+      target.setCustomValidity('От 30 и больше');
+    } else {
+      target.setCustomValidity('');
+    }
+  });
+
+  // Валидация адреса
+  addressField.addEventListener('invalid', function () {
+    if (!addressField.validity.valid) {
+      if (addressField.validity.valueMissing) {
+        addressField.setCustomValidity('Тут надо обязательно написать адрес, иначе никак');
+      }
+    } else {
+      addressField.setCustomValidity('');
+    }
+  });
+
+  // Валидация цены
+  priceField.addEventListener('invalid', function () {
+    if (!priceField.validity.valid) {
+      if (priceField.validity.valueMissing) {
+        priceField.setCustomValidity('Тут надо обязательно указать цену, иначе никак');
+      } else if (priceField.validity.rangeOverflow) {
+        priceField.setCustomValidity('Меньше 1000000 больше 0');
+      } else if (priceField.validity.rangeUnderflow ) {
+        priceField.setCustomValidity('Больше 0 меньше 1000000');
+      }
+    } else {
+      priceField.setCustomValidity('');
+    }
+  });
 
 })();
--- js/synchronize-fields.js
+++ js/synchronize-fields.js
@@ -8,11 +8,31 @@
       secondField.value = firstField.value;
     },
 
-    cycleValues: function (firstField, firstArr, secondField, secondArr) {
-      for (var i = 0; i < firstArr.length; i++) {
-        if (firstField.value === firstArr[i]) {
-          secondField.value = secondArr[i];
-        }
+    minValues: function (firstField, firstArr, secondField, secondArr) {
+      var index = firstArr.indexOf(firstField.value);
+      secondField.min = secondArr[index];
+      secondField.value = secondField.min;
+    },
+
+    minValues: function (firstField, firstArr, secondField, secondArr) {
+      var index = firstArr.indexOf(firstField.value);
+      secondField.min = secondArr[index];
+      secondField.value = secondField.min;
+    },
+
+    indexValues: function (firstField, secondField) {
+      var index = firstField.selectedIndex;
+      if (index === 0) {
+        secondField.selectedIndex = 2;
+      }
+      if (index === 1) {
+        secondField.selectedIndex = 1;
+      }
+      if (index === 2) {
+        secondField.selectedIndex = 0;
+      }
+      if (index === 3) {
+        secondField.selectedIndex = 3;
       }
     }
 
