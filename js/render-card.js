f35ac198ac9c546e0aa73bfac07eca6b961fc98e
margarita-p <dayzzzy@gmail.com>
2017-09-17 09:02:33 +0300

    second-master

--- js/render-card.js
+++ js/render-card.js
@@ -2,6 +2,7 @@
 
 (function () {
 
+  var PRICE_END = '₽/ночь';
   var RUS_TYPES = {
     'flat': 'Квартира',
     'house': 'Дом',
@@ -17,14 +18,16 @@
 
     lodgeElement.querySelector('.lodge__title').textContent = arr.offer.title;
     lodgeElement.querySelector('.lodge__address').textContent = arr.offer.address;
-    lodgeElement.querySelector('.lodge__price').innerHTML = arr.offer.price;
+    lodgeElement.querySelector('.lodge__price').textContent = arr.offer.price + PRICE_END;
     lodgeElement.querySelector('.lodge__type').textContent = RUS_TYPES[arr.offer.type];
     lodgeElement.querySelector('.lodge__rooms-and-guests').textContent = 'Для ' + arr.offer.guests + ' гостей в ' + arr.offer.rooms + ' комнатах';
     lodgeElement.querySelector('.lodge__checkin-time').textContent = 'Заезд после ' + arr.offer.checkin + ' , выезд до ' + arr.offer.checkout;
 
     var userFeatures = arr.offer.features;
     for (var i = 0; i < userFeatures.length; i++) {
-      lodgeElement.querySelector('.lodge__features').innerHTML += '<span class = "feature__image feature__image--' + userFeatures[i] + '"></span>';
+      var newElement = document.createElement('span');
+      newElement.className = 'feature__image ' + 'feature__image--' + userFeatures[i];
+      lodgeElement.querySelector('.lodge__features').appendChild(newElement);
     }
 
     lodgeElement.querySelector('.lodge__description').textContent = arr.offer.description;
